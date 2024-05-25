20ce466ae85b6286075f9273fec62a5a6f7f26ef
ymzksgkz <sy20151201@gmail.com>
2024-05-25 17:49:00 +0900

    change toggleterm settings

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,4 +1,4 @@
--- setting lazy.nvim
+-- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
--- neovim/.config/nvim/lua/plugins/toggleterm.lua
+++ neovim/.config/nvim/lua/plugins/toggleterm.lua
@@ -1,4 +1,24 @@
 return {
-  {'akinsho/toggleterm.nvim', version = "*", config = true}
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = function()
+      local Terminal = require('toggleterm.terminal').Terminal
+      local lazygit = Terminal:new({
+        cmd = "lazygit",
+        hidden = true,
+        direction = "float",
+        float_opts = {
+          border = "single"
+        }
+      })
+
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
+
+      vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+    end
+  }
 }
 
