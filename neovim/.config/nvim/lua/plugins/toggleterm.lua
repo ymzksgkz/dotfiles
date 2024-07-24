bfb2b4f33047670323fe54d734eeba6b5a39d634
ymzksgkz <83740740+ymzksgkz@users.noreply.github.com>
2024-07-25 00:03:30 +0900

    change toggleterm settings
    add floating shell
    add lazydocker

--- neovim/.config/nvim/lua/plugins/toggleterm.lua
+++ neovim/.config/nvim/lua/plugins/toggleterm.lua
@@ -4,6 +4,8 @@ return {
     version = "*",
     config = function()
       local Terminal = require('toggleterm.terminal').Terminal
+
+      -- lazygit(\+lag)
       local lazygit = Terminal:new({
         cmd = "lazygit",
         hidden = true,
@@ -12,12 +14,43 @@ return {
           border = "single"
         }
       })
-
       function _lazygit_toggle()
         lazygit:toggle()
       end
+      vim.api.nvim_set_keymap("n", "<leader>lag", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
 
-      vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+      
+      -- lazydocker(\+lad)
+      local lazydocker = Terminal:new({
+        cmd = "lazydocker",
+        hidden = true,
+        direction = "float",
+        float_opts = {
+          border = "single"
+        }
+      })
+      function _lazydocker_toggle()
+        lazydocker:toggle()
+      end
+      vim.api.nvim_set_keymap("n", "<leader>lad", "<cmd>lua _lazydocker_toggle()<CR>", {noremap = true, silent = true})
+
+
+      -- Terminal(\\)
+      local shell = Terminal:new({
+        cmd = "zsh",
+        hidden = true,
+        -- direction = "float",
+        direction = "float",
+        float_opts = {
+          border = "single",
+        }
+      })
+
+      function _shell_toggle()
+        shell:toggle()
+      end
+
+      vim.api.nvim_set_keymap("n", "<leader><leader>", "<cmd>lua _shell_toggle()<CR>", {noremap = true, silent = true})
     end
   }
 }
