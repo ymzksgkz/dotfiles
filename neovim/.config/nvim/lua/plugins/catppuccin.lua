0e800841d7bac2e1596be0df32ffee7b51edaeae
ymzksgkz <83740740+ymzksgkz@users.noreply.github.com>
2024-07-24 23:04:14 +0900

    change theme

--- neovim/.config/nvim/lua/plugins/catppuccin.lua
+++ neovim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,5 +1,10 @@
 return {
   "catppuccin/nvim",
   name = "catppuccin",
-  priority = 1000
+  priority = 1000,
+  config = function()
+    --vim.cmd.colorscheme "catppuccin"
+    vim.cmd.colorscheme "catppuccin-mocha"
+
+  end,
 }
