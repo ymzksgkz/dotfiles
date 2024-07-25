cced8f02155b117da17b0a088a3e4f5898f58077
ymzksgkz <83740740+ymzksgkz@users.noreply.github.com>
2024-07-26 00:01:49 +0900

    install bamboo

---
+++ neovim/.config/nvim/lua/plugins/bamboo.lua
@@ -0,0 +1,12 @@
+-- Using lazy.nvim
+return {
+  'ribru17/bamboo.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('bamboo').setup {
+      -- optional configuration here
+    }
+    require('bamboo').load()
+  end,
+}
